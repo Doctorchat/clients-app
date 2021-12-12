3edcd28f0ed7ef19074cf6c486f343e612980eed
Denis <denisnovac2001@mail.ru>
2021-12-12 12:16:33 +0200

    fixed next build

---
+++ pages/chat/[id].js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+export default function ChatContent(props) {
+  //  const {  } = props;
+
+  return "Chat content";
+}
+
+ChatContent.propTypes = {};
+
+ChatContent.defaultProps = {};
