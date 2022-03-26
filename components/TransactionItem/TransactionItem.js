c5c947689f4446e9dbab16769ef48a2e2068270f
Novac Denis <denisnovac2017@gmail.com>
2022-03-26 13:35:09 +0200

    fix

--- components/TransactionItem/TransactionItem.js
+++ components/TransactionItem/TransactionItem.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import { useCallback } from "react";
-import { useTranslation } from "react-i18next";
 import Button from "../Button";
-import cs from "@/utils/classNames";
+import i18next from "@/services/i18next";
 import useTabsContext from "@/packages/Tabs/hooks/useTabsContext";
 import { leftSideTabs } from "@/context/TabsKeys";
 import date from "@/utils/date";
+import cs from "@/utils/classNames";
 import CamIcon from "@/icons/webcam.svg";
 import CheckIcon from "@/icons/check.svg";
 import TimesIcon from "@/icons/times.svg";
@@ -36,23 +36,40 @@ const statuses = {
 const categories = {
   meet: <CamIcon />,
   message: <FontIcon />,
+  withdraw: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+};
+
+const categoriesText = {
+  withdraw: i18next.t("withdrawal"),
+  meet: i18next.t("online_meet"),
+  message: i18next.t("simple_message"),
 };
 
 function TransactionItem(props) {
   const { type, category, amount, status, created } = props;
   const { updateTabsConfig } = useTabsContext();
-  const { t } = useTranslation();
 
   const goToBonuses = useCallback(() => updateTabsConfig(leftSideTabs.bonuses), [updateTabsConfig]);
 
   return (
     <div className="transaction-item">
-      <div className={cs("transaction-category", category)}>{categories[category]}</div>
+      <div className={cs("transaction-category message", category)}>{categories[category]}</div>
       <div className="transaction-caption">
         <h4 className="transaction-title">
-          <span className="category-title">
-            {category === "meet" ? t("online_meet") : t("simple_message")}
-          </span>
+          <span className="category-title">{categoriesText[category]}</span>
           {status === "cancel" ? (
             <Button onClick={goToBonuses} size="sm">
               Bonus +1
