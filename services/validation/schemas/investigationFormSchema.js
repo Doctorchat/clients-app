c1b56ff137278f5b23cdd98f705ae1a11579cf26
Novac Denis <denisnovac2017@gmail.com>
2022-10-03 20:11:41 +0300

    made all investigation form fields required

--- services/validation/schemas/investigationFormSchema.js
+++ services/validation/schemas/investigationFormSchema.js
@@ -2,11 +2,13 @@ import { number } from "yup";
 import { string, object } from "yup";
 
 const investigationFormSchema = object().shape({
+  name: string().required(),
   age: number().min(0).required(),
   weight: number().min(0).required(),
   height: number().min(0).required(),
   location: string().required(),
-  activity: string(),
+  activity: string().required(),
+  sex: object().required(),
   epidemiological: object().required(),
   diseases: object().required(),
   diseases_spec: string().required(),
