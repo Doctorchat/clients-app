93ccc9ba42dcd638efd86d926a3f8c62147ae30a
Novac Denis <denisnovac2017@gmail.com>
2023-02-06 17:08:34 +0200

    fixed client investigation form schema

--- services/validation/schemas/investigationFormSchema.js
+++ services/validation/schemas/investigationFormSchema.js
@@ -5,25 +5,18 @@ import i18next from "@/services/i18next";
 import isValidSelectOption from "@/utils/isValidSelectOption";
 
 const investigationFormSchema = object().shape({
-  name: string().required(),
-  age: number().min(0).required(),
-  weight: number().min(0).required(),
-  height: number().min(0).required(),
-  location: string().required(),
-  activity: string().required(),
+  name: string().required(), //
+  age: number().min(0).required(), //
+  weight: number().min(0).required(), //
+  height: number().min(0).required(), //
+  location: string().required(), //
+  activity: string().required(), //
   sex: mixed()
     .required()
     .test({
       name: "select-validation",
       message: i18next.t("yup_mixed_required"),
-      test: (value) => isValidSelectOption(value),
-    }),
-  diseases: mixed()
-    .required()
-    .test({
-      name: "select-validation",
-      message: i18next.t("yup_mixed_required"),
-      test: (value) => isValidSelectOption(value),
+      test: (value) => isValidSelectOption(value), //
     }),
   diseases_spec: string().required(),
 });
