1fc7de34dadbb46a11e1ebc07f0d69540301f025
cralex11 <cristeaalex7@gmail.com>
2023-04-21 13:30:08 +0300

    DOC-479: fixed gtm , build

--- pages/_document.js
+++ pages/_document.js
@@ -12,29 +12,27 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
           />
-          {process.env.NODE_ENV === "production" &&
-            process.env.NEXT_PUBLIC_ENABLE_GOOGLE_TAG != 0 && (
-              <Script id="google-tag" strategy="afterInteractive">
-                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          {process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_GTM_KEY && (
+            <Script id="google-tag" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W5ZWSFM');`}
-              </Script>
-            )}
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_KEY}');`}
+            </Script>
+          )}
         </Head>
         <body>
-          {process.env.NODE_ENV === "production" &&
-            process.env.NEXT_PUBLIC_ENABLE_GOOGLE_TAG != 0 && (
-              <noscript>
-                <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=GTM-W5ZWSFM"
-                  height="0"
-                  width="0"
-                  style={{ display: "none", visibility: "hidden" }}
-                />
-              </noscript>
-            )}
+          {process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_GTM_KEY && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_KEY}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          )}
           <Main />
           <NextScript />
         </body>
