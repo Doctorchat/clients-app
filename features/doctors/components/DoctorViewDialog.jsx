73eb8ccd2bb551578596c82a196c836fc5e3379f
Alexandru Cristea <45730710+cralex11@users.noreply.github.com>
2023-05-01 19:16:29 +0300

    DOC-347: added tabs for review (#43)
    * DOC-347: added tabs for review
    
    * DOC-347: changed alias

--- features/doctors/components/DoctorViewDialog.jsx
+++ features/doctors/components/DoctorViewDialog.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Tabs } from "antd";
 import PropTypes from "prop-types";
 import { useBoolean } from "usehooks-ts";
 
 import { IconBtn } from "@/components/Button";
 import DcTooltip from "@/components/DcTooltip";
 import Popup from "@/components/Popup";
+import ReviewsList from "@/components/ReviewsList";
 import Skeleton from "@/components/Skeleton";
 import ConsultationOptions from "@/features/doctors/components/ConsultationOptions";
 import useCurrency from "@/hooks/useCurrency";
@@ -91,6 +93,19 @@ export const DoctorViewDialog = ({ doctor, isLoading, onClose, onMessageTypeClic
     onHideModal();
   }, [onHideModal, onClose]);
 
+  const items = React.useMemo(() => [
+    {
+      key: "1",
+      label: t("general_information"),
+      children: <GeneralInfo doctor={doctor} />,
+    },
+    {
+      key: "2",
+      label: t("reviews") + ` (${doctor?.reviews?.length})`,
+      children: <ReviewsList data={doctor?.reviews} />,
+      disabled: !doctor?.reviews?.length,
+    },
+  ]);
   return (
     <Popup className="doctor-view__modal" visible={isOpen} onVisibleChange={onVisibleChange}>
       {isLoading ? (
@@ -117,25 +132,7 @@ export const DoctorViewDialog = ({ doctor, isLoading, onClose, onMessageTypeClic
               onVideoTypeClick={onVideoTypeClick}
               onMessageTypeClick={onMessageTypeClick}
             />
-
-            <p className="doctor-view__description">{getPropByLangOrThrow(doctor.about?.bio, getActiveLng())}</p>
-
-            <div className="doctor-view__activity">
-              <div className="doctor-view__activity-item">
-                <h4 className="doctor-view__activity-title">{t("work")}:</h4>
-                <p className="doctor-view__activity-description">
-                  <HospitalIcon />
-                  <span>{doctor.activity.workplace}</span>
-                </p>
-              </div>
-              <div className="doctor-view__activity-item">
-                <h4 className="doctor-view__activity-title">{t("education")}:</h4>
-                <p className="doctor-view__activity-description">
-                  <GraduationIcon />
-                  {doctor.activity.education[0]}
-                </p>
-              </div>
-            </div>
+            <Tabs defaultActiveKey="1" items={items} type="card" className="change-tabs-colors" />
           </div>
         </article>
       )}
@@ -197,5 +194,32 @@ const Badges = ({ doctor }) => {
         </span>
       </DcTooltip>
     </div>
+  );
+};
+
+const GeneralInfo = ({ doctor }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <p className="doctor-view__description">{getPropByLangOrThrow(doctor.about?.bio, getActiveLng())}</p>
+
+      <div className="doctor-view__activity">
+        <div className="doctor-view__activity-item">
+          <h4 className="doctor-view__activity-title">{t("work")}:</h4>
+          <p className="doctor-view__activity-description">
+            <HospitalIcon />
+            <span>{doctor.activity.workplace}</span>
+          </p>
+        </div>
+        <div className="doctor-view__activity-item">
+          <h4 className="doctor-view__activity-title">{t("education")}:</h4>
+          <p className="doctor-view__activity-description">
+            <GraduationIcon />
+            {doctor.activity.education[0]}
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
