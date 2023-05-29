9340d226da6f6412bbada0cd57487d8b68966602
Novac Denis <denisnovac2017@gmail.com>
2023-05-29 11:31:38 +0300

    fixed ChatContentAccept validation

--- modules/common/ChatContent/ChatContentFooter.js
+++ modules/common/ChatContent/ChatContentFooter.js
@@ -16,25 +16,32 @@ export default function ChatContentFooter(props) {
   const { openMessageFormPopup, status, chatId, paymentUrl, type, isAccepted } = props;
   const { t } = useTranslation();
 
-  const isAcceptedFalse = React.useMemo(() => type !== "support" && !isAccepted, [isAccepted, type]);
+  const isChatContentAcceptVisible = React.useMemo(() => {
+    if (status && ["initied", "unpaid", "closed"].includes(status)) return false;
+    if (type === "support") return false;
+    if (isAccepted === false) return true;
+
+    return false;
+  }, [isAccepted, status, type]);
 
   const isMessageBarVisible = React.useMemo(() => {
     if (status && ["initied", "unpaid", "closed"].includes(status)) return false;
-    if (user?.data?.role === userRoles.get("doctor") && isAcceptedFalse) return false;
+    if (user?.data?.role === userRoles.get("doctor") && isAccepted === false) return false;
 
     return true;
-  }, [isAcceptedFalse, status, user?.data?.role]);
+  }, [isAccepted, status, user?.data?.role]);
 
   return (
     <>
-      <AuthRoleWrapper extraValidation={status === "initied"} roles={[userRoles.get("client")]}>
+      <AuthRoleWrapper extraValidation={status && status === "initied"} roles={[userRoles.get("client")]}>
         <div className="chat-content-start w-100 d-flex justify-content-center">
           <Button type="text" onClick={openMessageFormPopup}>
             {t("start_conversation")}
           </Button>
         </div>
       </AuthRoleWrapper>
-      <AuthRoleWrapper extraValidation={status === "unpaid"} roles={[userRoles.get("client")]}>
+
+      <AuthRoleWrapper extraValidation={status && status === "unpaid"} roles={[userRoles.get("client")]}>
         <div className="chat-content-start w-100 d-flex justify-content-center">
           <a href={paymentUrl}>
             <Button type="text" className="confirm-cancel-btn">
@@ -44,7 +51,7 @@ export default function ChatContentFooter(props) {
         </div>
       </AuthRoleWrapper>
 
-      <AuthRoleWrapper roles={[userRoles.get("doctor")]} extraValidation={isAcceptedFalse}>
+      <AuthRoleWrapper extraValidation={isChatContentAcceptVisible} roles={[userRoles.get("doctor")]}>
         <ChatContentAccept chatId={chatId} />
       </AuthRoleWrapper>
 
