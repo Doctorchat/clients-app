553ec25d425870ffcf33309a4d207a4f2eb08580
Novac Denis <denisnovac2017@gmail.com>
2023-05-31 17:59:46 +0300

    fixed home page domain, open home page in a new tab

--- pages/doctors.js
+++ pages/doctors.js
@@ -60,7 +60,7 @@ export default function Doctors() {
           overlay={
             <Menu>
               <Menu.Item icon={<HomeIcon />} className="home-item">
-                <Link href={HOME_PAGE_URL}>
+                <Link href={HOME_PAGE_URL} target="_blank" rel="noreferrer noopener">
                   {t("home_page")}
                 </Link>
               </Menu.Item>
