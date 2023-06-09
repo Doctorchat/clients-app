2c323ccc9d743406b62bb76a2694f7e8bd56c4ef
Novac Denis <denisnovac2017@gmail.com>
2023-06-09 17:43:05 +0300

    removed destroy conversation call

--- pages/registration-flow/message/[chatId].js
+++ pages/registration-flow/message/[chatId].js
@@ -1,20 +1,10 @@
 import React from "react";
-import { useRouter } from "next/router";
 
 import AuthWrapper from "@/containers/AuthWrapper";
 import { Layout, MessageForm } from "@/features/registration-flow";
-import api from "@/services/axios/api";
 import i18next from "@/services/i18next";
 
 export default function MessagePage() {
-  const router = useRouter();
-
-  React.useEffect(() => {
-    return () => {
-      api.conversation.destroy(router.query.chatId).catch(() => {});
-    };
-  }, [router.query.chatId]);
-
   return <MessageForm />;
 }
 
