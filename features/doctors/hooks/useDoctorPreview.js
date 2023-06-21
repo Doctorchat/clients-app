e67303a9b1cd19915815d980134b3715458c647e
Novac Denis <45555668+novacdenis@users.noreply.github.com>
2023-06-21 17:30:36 +0300

    fixed undefined doctor (#92)

--- features/doctors/hooks/useDoctorPreview.js
+++ features/doctors/hooks/useDoctorPreview.js
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
+
+import { notification } from "@/store/slices/notificationsSlice";
 
 import { getDoctor } from "../api";
 
 const useDoctorPreview = (defaultDoctorPreviewId) => {
   const [doctorPreviewId, setDoctorPreviewId] = useState(defaultDoctorPreviewId);
 
+  const dispatch = useDispatch();
+
   const { data, isLoading } = useQuery(
     ["doctor-view", doctorPreviewId],
-    () => getDoctor(doctorPreviewId),
-    { enabled: Boolean(doctorPreviewId) }
+    async () => {
+      try {
+        const response = await getDoctor(doctorPreviewId);
+        return response;
+      } catch {
+        dispatch(notification({ type: "error", title: "error", descrp: "doctor_unavailable" }));
+        setDoctorPreviewId(null);
+        return null;
+      }
+    },
+    {
+      enabled: Boolean(doctorPreviewId),
+      retry: false,
+    }
   );
 
   useEffect(() => {
