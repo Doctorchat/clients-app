14c226a41b6ce219b6350b520f8921aa68299007
anDarya <dary.andronic.office@gmail.com>
2023-09-26 08:52:35 +0300

    Add console

--- containers/AuthWrapper.js
+++ containers/AuthWrapper.js
@@ -41,9 +41,9 @@ export default function AuthWrapper(props) {
         const { title, body, click_action, icon } = payload.data;
 
         // Create a new notification
-        console.log(router)
         const { id, pathname } = router.query;
-        if(pathname !== '/chat'){
+        console.log(router, pathname !== "/chat");
+        if (pathname !== "/chat") {
           const notification = new Notification(title, { body });
           // Set the notification icon if provided
           if (icon) {
@@ -54,7 +54,7 @@ export default function AuthWrapper(props) {
           if (click_action) {
             window.location.href = click_action;
           }
-        }       
+        }
       });
 
       return () => {
